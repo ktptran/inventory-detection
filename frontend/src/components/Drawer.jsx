4f45bbac0817011bd708448edca4590108a27948
Kevin Tran <kevint24@uw.edu>
2024-02-13 11:50:51 -0800

    refact: added in side drawer

--- frontend/src/components/Drawer.jsx
+++ frontend/src/components/Drawer.jsx
@@ -1,6 +1,5 @@
 import Drawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
@@ -10,24 +9,20 @@ function MuiDrawer({ isDrawerOpen, setIsDrawerOpen }) {
     <>
       <Drawer anchor="left" open={isDrawerOpen}>
         <Box
-          width="250px"
+          width="200px"
           pt={2}
-          sx={{ textAlign: 'center', display: 'flex', justifyContent: 'left' }}
+          sx={{ textAlign: 'center', display: 'flex', justifyContent: 'right' }}
           role="presentation"
         >
           <Box width="50px" onClick={() => setIsDrawerOpen(false)}>
             <ArrowBackIosIcon />
           </Box>
-
-          <Typography variant="h5" component="div">
-            Side Panel
-          </Typography>
         </Box>
         <Box pt={2} paddingBottom={2}>
           <Divider />
         </Box>
         {/* Routing */}
-        <Button color="inherit" href="/">
+        <Button color="inherit" href="/home">
           Home
         </Button>
         <Button color="inherit" href="/inventory">
