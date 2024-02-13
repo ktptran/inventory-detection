a8a10b45290b48942b7bc8ae410392cb2baa0461
Kevin Tran <kevint24@uw.edu>
2024-02-13 11:53:01 -0800

    bug: inspection bugs with icon onClick event now in iconButton onClick

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,7 +5,6 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import Avatar from '@mui/material/Avatar';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -22,8 +21,15 @@ export default function NavBar({ setIsDrawerOpen, setUser }) {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon onClick={() => setIsDrawerOpen(true)} />
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={() => setIsDrawerOpen(true)}
+          >
+            <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Refrigerator Inventory Detection
