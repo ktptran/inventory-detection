b650641050e8a2878bacd60745a9d0892c92ba7a
Kevin Tran <kevint24@uw.edu>
2024-02-23 09:46:02 -0800

    bug: user state pulling from Cognito once refreshing web console

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import CssBaseline from '@mui/material/CssBaseline';
-import React, { useMemo, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
 import Drawer from './components/Drawer';
 import Footer from './components/Footer';
@@ -11,27 +11,25 @@ import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
 import Refrigerator from './pages/Refrigerator';
 
+import { fetchAuthSession } from 'aws-amplify/auth';
 import { UserContext } from './context/UserContext';
-// import { fetchAuthSession } from 'aws-amplify/auth';
 
 function App() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [user, setUser] = useState(null);
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const value = useMemo(() => ({ user, setUser }), [user, setUser]);
 
-  // useEffect(() => {
-  //   if (!user) {
-  //     const fetchSession = async () => await fetchAuthSession();
-  //     const session = fetchSession();
-  //     if (session) {
-  //       setUser(session);
-  //     } else {
-  //       navigate.push('/login');
-  //     }
-  //   }
-  // }, [navigate, user]);
+  useEffect(() => {
+    if (!user) {
+      const fetchSession = async () => {
+        const session = await fetchAuthSession();
+        session['tokens'] ? setUser(session) : navigate('/');
+      };
+      fetchSession();
+    }
+  }, [navigate, user]);
 
   return (
     <UserContext.Provider value={value}>
